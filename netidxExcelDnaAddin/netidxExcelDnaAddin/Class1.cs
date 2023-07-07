ec4053bcbd2c9b55b467bf82dd1b5de33782b942
MegaPikachu <1025151692@qq.com>
2023-07-07 11:38:15 +0800

    update Excel DNA addin: add netidx dll lib to the path

--- netidxExcelDnaAddin/netidxExcelDnaAddin/Class1.cs
+++ netidxExcelDnaAddin/netidxExcelDnaAddin/Class1.cs
@@ -11,13 +11,13 @@ namespace netidxExcelDnaAddin
     public class Class1
     {
         // 声明Rust DLL中的函数签名
-        [DllImport("netidx_excel.dll")]
+        [DllImport("lib/netidx_excel.dll")]
         public static extern string write_value_string(string path, string value);
 
-        [DllImport("netidx_excel.dll")]
+        [DllImport("lib/netidx_excel.dll")]
         public static extern string write_value_int(string path, int value);
 
-        [DllImport("netidx_excel.dll")]
+        [DllImport("lib/netidx_excel.dll")]
         public static extern string write_value_float(string path, double value);
 
 
