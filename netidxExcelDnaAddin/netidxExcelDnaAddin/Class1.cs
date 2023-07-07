67e7ae2c22288745b637dcd41bcb8c959b3e67d1
MegaPikachu <1025151692@qq.com>
2023-07-07 10:45:20 +0800

    update the csharp excel dna addin file: rename and remove unused files

---
+++ netidxExcelDnaAddin/netidxExcelDnaAddin/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+using System.Runtime.InteropServices;
+
+namespace netidxExcelDnaAddin
+{
+    public class Class1
+    {
+        // 声明Rust DLL中的函数签名
+        [DllImport("netidx_excel.dll")]
+        public static extern string write_value_string(string path, string value);
+
+        [DllImport("netidx_excel.dll")]
+        public static extern string write_value_int(string path, int value);
+
+        [DllImport("netidx_excel.dll")]
+        public static extern string write_value_float(string path, double value);
+
+
+        [ExcelFunction(Description = "Write data to netidx container")]
+        public static string PublishData(string path, object value)
+        {
+            if (value is int)
+            {
+                return write_value_int(path, (int)value);
+            }
+            else if (value is double || value is float)
+            {
+                return write_value_float(path, (double)value);
+            }
+            else if (value is string)
+            {
+                return write_value_string(path, (string)value);
+            }
+            else
+            {
+                return "input value is not an int, double or string";
+            }
+        }
+    }
+}
