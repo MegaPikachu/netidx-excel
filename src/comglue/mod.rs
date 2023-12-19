6de094aa80e6d78806fda4a122776065e9410fbb
Tiancheng Tang <ttang@3RIVERS.GROUP>
2023-12-19 10:53:43 +0800

    auto remove old logs

--- src/comglue/mod.rs
+++ src/comglue/mod.rs
@@ -34,6 +34,37 @@ impl Default for Config {
     }
 }
 
+fn clean_old_logs(folder_path: PathBuf) -> Result<()> {
+    let today = chrono::Utc::now();
+    let one_week_ago = today - chrono::Duration::weeks(1);
+    for file in fs::read_dir(folder_path)? {
+        let file_path = file?.path();
+        if let Some(file_name) = file_path.file_name() {
+            let file_name_str = file_name.to_string_lossy();
+            if file_name_str.starts_with("log.")
+                && file_name_str.ends_with(".txt")
+                && file_name_str.len() == 28
+            {
+                let date_str = &file_name_str[4..23];
+                let date = match chrono::DateTime::parse_from_str(
+                    &format!("{date_str}+0000"),
+                    "%Y-%m-%dT%H-%M-%S%z",
+                ) {
+                    Ok(date) => date,
+                    Err(_) => {
+                        continue;
+                    } // ignore invalid filenames
+                };
+                if date < one_week_ago {
+                    log::info!("Deleting file: {:?}", file_path);
+                    fs::remove_file(file_path)?;
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
 fn load_config_and_init_log() -> Result<Config> {
     let path = match dirs::config_dir() {
         Some(d) => d,
@@ -47,6 +78,7 @@ fn load_config_and_init_log() -> Result<Config> {
     fs::create_dir_all(log_base.clone())?;
     let config_file = base.join("config.json");
     let now = chrono::Utc::now().format("%Y-%m-%dT%H-%M-%S").to_string();
+
     let log_file = log_base.join(format!("log.{}Z.txt", now));
     if !config_file.exists() {
         fs::write(&*config_file, &serde_json::to_string_pretty(&Config::default())?)?;
@@ -54,6 +86,9 @@ fn load_config_and_init_log() -> Result<Config> {
     let config: Config = serde_json::from_str(&fs::read_to_string(config_file.clone())?)?;
     let log = File::create(log_file)?;
     simplelog::WriteLogger::init(config.log_level, simplelog::Config::default(), log)?;
+    if let Err(err) = clean_old_logs(log_base.clone()) {
+        log::error!("Clean old logs error, err msg = {}", err);
+    };
     Ok(config)
 }
 
